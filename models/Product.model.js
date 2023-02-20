0195090db2cf9b03e1b15191cbce92c237be49bd
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-02-20 17:44:26 +0530

    uypdate

--- models/Product.model.js
+++ models/Product.model.js
@@ -4,7 +4,7 @@
  const ProductSchema= mongoose.Schema({
      description:String,
      title:String,
-     pic:String,
+     pic:[],
      userId:String,
      postedby:{type:mongoose.Types.ObjectId,ref:"user",required:true}
  },{
